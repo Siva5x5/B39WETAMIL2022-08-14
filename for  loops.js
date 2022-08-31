1f50ae600385723e1e6b552525289f7227007c8a
Siva5x5 <110763932+Siva5x5@users.noreply.github.com>
2022-09-01 04:31:51 +0530

    Add files via upload

---
+++ for  loops.js
@@ -0,0 +1,35 @@
+// for loop:
+let clock = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+for(var i = 0; i < clock.length; i++) {
+    console.log(clock[i]);
+}
+
+// ----------------------------------------------------------- //
+
+// for of loop:
+let calender = [ "january", "february", "march", "april", "may", "jun", 
+               "july", "august", "septemper", "october", "novomber", "december"]
+for (let months of calender) {
+    console.log(months);
+}
+
+
+
+// ----------------------------------------------------------- //
+
+// for in loop:
+
+const movie = {
+    name: "ayirathil oruvan",
+    hero: "karthi",
+    heroine1: "andrea",
+    heroine2: "reemasen",
+    director: "selvaraaghaavan",
+    music: "g.v.prkash kumar"
+}
+
+for (let details in movie) {
+
+    let value = movie[details];
+    console.log(details + "--->" + value);
+}
